ed8ea9ad25243a7e5608111285bd8fca94221898
Christopher Watson <christopher_watson@icloud.com>
2019-09-23 21:31:43 -0400

    testimonial isabella

--- src/components/testimonial.js
+++ src/components/testimonial.js
@@ -41,16 +41,21 @@ const Testimonial = () => {
       <div style={styles.text} className="testimonial-text">
         Christopher has curated the perfect website for me. It’s captivating, to the point, and relays exactly the message I was looking to communicate to my audience. 
         I would definitely look to him to create another in the future. <br/>
-        <span style={styles.author}className="testimonial-author"> - Lindsay Watson | <a style={styles.links} target='_blank' rel="noopener noreferrer" href="http://lindsaywatson.co">lindsaywatson.co</a></span>
+        <span style={styles.author}className="testimonial-author"> - Lindsay Watson | <a style={styles.links} target='_blank' rel="noopener noreferrer" href="https://lindsaywatson.co">lindsaywatson.co</a></span>
       </div>
-      {/* <hr style={styles.hr}/> */}
+
+
+      <hr style={styles.hr}/>
 
       {/* test 2 */}
-      {/* <div style={styles.text} className="testimonial-text">
-        Sociosqu accumsan nec varius lectus cum sociis rutrum tincidunt facilisi magna eleifend congue parturient ac ante nam posuere orci aptent <br/>
-        <span style={styles.author}className="testimonial-author"> - Mike Jones</span>
+      <div style={styles.text} className="testimonial-text">
+      Chris made me a website that I have been wanting for years. Simplicity at its best. It is super functional, easy to use, mobile friendly, and modern. Chris was very attentive to my wants and needs. He was also able to implement his own style into the website when I was unsure about something. Chris was very easy to work with and was always responsive to my messages.  <br/>
+        <span style={styles.author}className="testimonial-author"> - Ben Isabella | <a style={styles.links} target='_blank' rel="noopener noreferrer" href="https://isabellafitness.com">isabellafitness.com</a></span>
       </div>
-      <hr style={styles.hr}/> */}
+
+
+      {/* <hr style={styles.hr}/> */}
+
 
       {/* test 3 */}
       {/* <div style={styles.text} className="testimonial-text">
